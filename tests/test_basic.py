2776f59a3e2a86fdb008599c3042bd772e60cae9
Sebastien Nicolet <snicolet95@gmail.com>
2024-12-22 16:14:20 +0100

    added tests for backward key

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from tests.models import Book, BookShelf
 from tortoise_serializer import Serializer
 
@@ -28,3 +30,26 @@ async def test_foreignkey_serialization():
     serializer = await BookSerializer.from_tortoise_orm(book)
     assert book.shelf.id == serializer.shelf.id
     assert book.shelf.name == serializer.shelf.name
+
+
+async def test_backward_fk_serialization():
+    class BookSerializer(Serializer):
+        id: int
+        title: str
+
+    class ShelfSerializer(Serializer):
+        id: int
+        name: str
+        books: list[BookSerializer]
+
+    shelf = await BookShelf.create(name="test shelf")
+    books = await asyncio.gather(
+        Book.create(title="A", shelf=shelf),
+        Book.create(title="B", shelf=shelf),
+        Book.create(title="C", shelf=shelf),
+    )
+    serializer = await ShelfSerializer.from_tortoise_orm(shelf)
+    assert len(serializer.books) == len(books)
+    for book_index, book in enumerate(books):
+        assert serializer.books[book_index].id == book.id
+        assert serializer.books[book_index].title == book.title
