eb4f5bd477e8c187295b185084d409e91a985a5e
Sebastien Nicolet <snicolet95@gmail.com>
2024-12-22 16:27:20 +0100

    more tests!

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -53,3 +53,60 @@ async def test_backward_fk_serialization():
     for book_index, book in enumerate(books):
         assert serializer.books[book_index].id == book.id
         assert serializer.books[book_index].title == book.title
+
+
+async def test_multiples_instances_serialization():
+    class BookSerializer(Serializer):
+        title: str
+
+    books = [await Book.create(title="A"), await Book.create(title="B")]
+    serializers = await BookSerializer.from_tortoise_instances(books)
+    assert isinstance(serializers, list)
+    assert len(serializers) == len(books)
+    for book_index, serializer in enumerate(serializers):
+        assert serializer.title == books[book_index].title
+
+
+async def test_instance_update():
+    class UpdateSerializer(Serializer):
+        title: str
+
+    shelf = await BookShelf.create(name="Testing")
+    book = await Book.create(title="test title", shelf=shelf)
+    serializer = UpdateSerializer(title="New title")
+    assert serializer.partial_update_tortoise_instance(book)
+    assert book.title == "New title"
+    assert book.shelf_id == shelf.id
+
+
+async def test_instance_creation():
+    class BookCreationSerializer(Serializer):
+        title: str
+        shelf_id: int
+
+    shelf = await BookShelf.create(name="Testing")
+    lotr_serializer = BookCreationSerializer(title="LOTR", shelf_id=shelf.id)
+    book = await lotr_serializer.create_tortoise_instance(Book)
+    assert book.id
+    assert book.title == "LOTR"
+    assert book.shelf_id == shelf.id
+
+
+async def test_from_queryset():
+    class BookSerializer(Serializer):
+        title: str
+
+    await Book.bulk_create(
+        [
+            Book(title="LOTR"),
+            Book(title="Harry Potter"),
+            Book(title="The Great Gatsby"),
+        ]
+    )
+    serializers = await BookSerializer.from_queryset(Book.all())
+    assert isinstance(serializers, list)
+    assert len(serializers) == 3
+    for index, title in enumerate(
+        ("LOTR", "Harry Potter", "The Great Gatsby")
+    ):
+        assert serializers[index].title == title
