33a86d0e9aab84705c5dacd891648ec7fa418867
Sebastien Nicolet <snicolet95@gmail.com>
2025-10-10 18:09:50 +0200

    updated dependencies

--- tortoise_serializer/__init__.py
+++ tortoise_serializer/__init__.py
@@ -8,7 +8,7 @@ from .serializers import (
 from .types import ContextType, Unset, UnsetType
 from .utils import ensure_fetched_fields
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 __all__ = [
     "ContextType",
     "ensure_fetched_fields",
