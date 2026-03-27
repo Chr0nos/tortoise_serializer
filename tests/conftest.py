04f16cac3b82d5e9713f4ea14054e61368ff4230
Sebastien Nicolet <snicolet95@gmail.com>
2026-03-27 12:42:52 +0100

    [1.8.0] upgrade to tortoise-orm 1.1.x & updated pydantic, got rid of poetry in favor of uv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,17 @@
-import pytest
-from tortoise import Tortoise
+import pytest_asyncio
+from tortoise.contrib.test import tortoise_test_context
+from typing import AsyncGenerator
+from tortoise.context import TortoiseContext
 
 
-@pytest.fixture(scope="session")
-def tortoise_config():
-    return {
-        "connections": {
-            "default": "sqlite://:memory:"  # Using in-memory SQLite for testing
-        },
-        "apps": {
-            "models": {
-                "models": ["tests.models"],
-                "default_connection": "default",
-            },
-        },
-    }
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def initialize_tortoise(tortoise_config):
-    """
-    Initialize Tortoise ORM for the test session.
-    """
-    await Tortoise.init(
-        db_url=tortoise_config["connections"]["default"],
-        modules={"models": tortoise_config["apps"]["models"]["models"]},
-    )
-    await Tortoise.generate_schemas()
-    yield
-    await Tortoise.close_connections()
-
-
-@pytest.fixture(scope="function", autouse=True)
-async def cleanup_db():
-    """
-    Clean up the database by truncating all tables after each test.
-    This ensures a clean state for subsequent tests.
-    """
-    for model in Tortoise.apps.get("models").values():
-        await model.all().delete()
+@pytest_asyncio.fixture(autouse=True)
+async def db() -> AsyncGenerator[TortoiseContext]:
+    async with tortoise_test_context(
+        modules=["tests.models"],
+        db_url="sqlite://:memory:",
+    ) as ctx:
+        await ctx.generate_schemas()
+        yield ctx
+        for app_models in ctx.apps.values():
+            for model in app_models.values():
+                await model.all().delete()
